ac4a2860343d4d90aab128ee937867236e92a16c
Nick <npiscitello@thelittlefish.net>
2019-02-02 19:39:02 -0500

    RTFM. The interrupt vector macro is TIMER1_OVF_vect, not TIM1_OVF_vect.

--- speaker_test/main.c
+++ speaker_test/main.c
@@ -16,23 +16,17 @@ uint8_t square_samples[] = {
 };
 uint8_t sample_index = 0;
 
-/*
-ISR(TIM1_OVF_vect) {
-  OCR1AL = samples[(sample_index++)%NUM_SAMPLES];
-}
-*/
-
-ISR(TIM0_OVF_vect) {
-  OCR0A = sine_samples[(sample_index++)%NUM_SAMPLES];
+ISR(TIMER1_OVF_vect) {
+  OCR1AL = sine_samples[(sample_index++)%NUM_SAMPLES];
 }
 
 
-int main() {
+
+int main(void) {
 
   // disable interrupts globally for setup
   cli();
 
-  /*
   // set OC1A (PB1) to output
   DDRB = 0x00 | _BV(DDB1);
 
@@ -54,34 +48,9 @@ int main() {
   TIFR1 = 0xFF;
   // clear OCR1AH b/c we're only dealing with 8 bit for now
   OCR1AH = 0x00;
-  */
-
-  // set OC1A (PB1) to output
-  DDRD = 0x00 | _BV(DDD6);
-
-  // turn on timer 0
-  PRR = 0xFF & !_BV(PRTIM0);
-
-  // PWM setup
-  // set pin behavior (non-inverted) and waveform mode (fast PWM, max resolution)
-  TCCR0A = 0x00 | _BV(COM0A1) | _BV(WGM01) | _BV(WGM00);
-  // clock prescaler (clk/1)
-  TCCR0B = 0x00 | _BV(CS00);
-  // clock prescaler (clk/8)
-  //TCCR0B = 0x00 | _BV(CS01);
-
-  // waveform setup (load new value to OCR1A every TOP interrupt)
-  // enable timer1 TOP interrupts and clear pending interrupts
-  //TIMSK0 = 0x00 | _BV(TOIE0);
-  //TIFR0 = 0xFF;
 
   // enable global interrupts
   sei();
 
-  while(1) {
-    // simulate an overflow interrupt
-    // this loop takes about 256 timer ticks to run
-    for(uint8_t j = 0; j < 0x52; j++) {}
-    OCR0A = square_samples[(sample_index++)%NUM_SAMPLES];
-  }
+  while(1) {}
 }
