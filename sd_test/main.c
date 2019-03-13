64c139928e51e6eb8f30c4478ffc1e0fa41138b4
Nick <npiscitello@thelittlefish.net>
2019-03-13 10:44:33 -0400

    testing compiling functions in main

--- sd_test/main.c
+++ sd_test/main.c
@@ -1,7 +1,17 @@
 #include <avr/io.h>
 #include "ff.h"
+#include "mmc_avr.h"
 
 int main(void) {
+
+  // call mmc_disk_timerproc every 10ms
+  mmc_disk_timerproc();
+
+  FIL* file;
+  void* voidptr;
+  UINT buf1;
+  UINT* buf2;
+  f_read(file, voidptr, buf1, buf2);
 
   while(1) {}
 }
